b46306e8121245013bed87074eb1747a14c78d6a
Bibek Paudel <bibekpaudel793@gmail.com>
2025-07-27 00:47:53 +0200

    Rename DbService.proto to UserService.proto and implement user management RPCs

--- src/main/java/sep3/cineflix/db_service/GrpcServices/UserServiceImpl.java
+++ src/main/java/sep3/cineflix/db_service/GrpcServices/UserServiceImpl.java
@@ -5,13 +5,10 @@ import org.springframework.stereotype.Service;
 import sep3.cineflix.db_service.Entities.User;
 import sep3.cineflix.db_service.Entities.UserRole;
 import sep3.cineflix.db_service.Repositories.UserRepository;
-import sep3.cineflix.grpc.CreateUserRequest;
-import sep3.cineflix.grpc.GetUserByEmailRequest;
-import sep3.cineflix.grpc.GetUserByUsernameRequest;
-import sep3.cineflix.grpc.UserResponse;
-import sep3.cineflix.grpc.UserServiceGrpc;
+import sep3.cineflix.grpc.*;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
@@ -29,9 +26,8 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
                 request.getUsername(),
                 request.getEmail(),
                 request.getHashedPassword(),
-                UserRole.valueOf(request.getUserRole())
+                UserRole.valueOf(request.getUserRole().toUpperCase())
         );
-
         User savedUser = userRepository.save(user);
         responseObserver.onNext(toUserResponse(savedUser));
         responseObserver.onCompleted();
@@ -59,6 +55,62 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
                 },
                 () -> responseObserver.onError(new RuntimeException("User not found"))
         );
+    }
+
+    @Override
+    public void getUserById(GetUserByIdRequest request, StreamObserver<UserResponse> responseObserver) {
+        Optional<User> userOpt = userRepository.findById(request.getId());
+        userOpt.ifPresentOrElse(
+                user -> {
+                    responseObserver.onNext(toUserResponse(user));
+                    responseObserver.onCompleted();
+                },
+                () -> responseObserver.onError(new RuntimeException("User not found"))
+        );
+    }
+
+    @Override
+    public void updateUser(UpdateUserRequest request, StreamObserver<UserResponse> responseObserver) {
+        Optional<User> userOpt = userRepository.findById(request.getId());
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            user.setUsername(request.getUsername());
+            user.setEmail(request.getEmail());
+            user.setHashedPassword(request.getHashedPassword());
+            user.setUserRole(UserRole.valueOf(request.getUserRole().toUpperCase()));
+            User updatedUser = userRepository.save(user);
+            responseObserver.onNext(toUserResponse(updatedUser));
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(new RuntimeException("User not found"));
+        }
+    }
+
+    @Override
+    public void deleteUser(DeleteUserRequest request, StreamObserver<DeleteUserResponse> responseObserver) {
+        if (userRepository.existsById(request.getId())) {
+            userRepository.deleteById(request.getId());
+            DeleteUserResponse response = DeleteUserResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("User deleted")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
+            responseObserver.onError(new RuntimeException("User not found"));
+        }
+    }
+
+    @Override
+    public void getAllUsers(GetAllUsersRequest request, StreamObserver<GetAllUsersResponse> responseObserver) {
+        var userResponses = userRepository.findAll().stream()
+                .map(this::toUserResponse)
+                .collect(Collectors.toList());
+        GetAllUsersResponse response = GetAllUsersResponse.newBuilder()
+                .addAllUsers(userResponses)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     private UserResponse toUserResponse(User user) {
